31a04b0369a1185bbac63b26ca33348310af6995
chenxudong <839823170@qq.com>
2024-02-29 16:38:36 +0800

    chore: 修改999000

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App ITQM"/>
-    2024/02/21
+    2024/02/2999999390-9099-9--9
   </div>
 </template>
 
